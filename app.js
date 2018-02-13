d3b3e82f3b8f8a8e403884e1ac8374ab6a09f20f
John Erickson <ericksonjohn08@hotmail.com>
2018-02-12 22:53:51 -0500

    Add test directory and expect and supertest libraries.

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+// ROUTES
+app.get('/api', (req, res) => {
+	res.send({greeting: 'Hello World!'});
+});
+
 module.exports = app;
---
+++ test/app_test.js
@@ -0,0 +1,19 @@
+// LIBRARY IMPORT
+const assert = require('assert');
+const expect = require('expect');
+const request = require('supertest');
+
+// LOCAL IMPORT
+const app = require('../app');
+
+// TESTS
+describe('The express app', () => {
+	it('Handles a GET request to /api', (done) => {
+		request(app)
+			.get('/api')
+			.end((err, res) => {
+				assert(res.body.greeting === 'Hello World!');
+				done();
+			});
+	});
+});
